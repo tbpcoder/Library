e06f13b1c3cd4f4fead87335d6d35c8fe2bfb0d1
thejas <thejas@cool.com>
2022-08-25 19:44:42 +0530

    finised project only left with grid not overflowing properly

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-function Book(name, author, pages,  read){
+function Book(name, author, pages,  read, bookno){
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.bookno = bookno;
 }
 
 let myLibrary = [];
@@ -11,25 +12,62 @@ function addBookToLibrary(bookname) {
     myLibrary.push(bookname);
 }
 
+function deletebook(bookid){
+    myLibrary.splice(bookid, 1)
+    for(var i = 0;i < myLibrary.length;i++){
+        myLibrary[i].bookno = i;
+    }
+    display();
+}
+
+function changeText(bookid){
+    
+    let status = myLibrary[bookid].read
+    if(status == "Read"){
+        myLibrary[bookid].read = "Not read"
+    }else{
+        myLibrary[bookid].read = "Read"
+    }
+
+    display()
+
+}
+
 Book.prototype.info = function(){
     var div = document.createElement('div')
     var title = document.createElement('p');
     var author = document.createElement('p')
     var pages = document.createElement('p')
     var read = document.createElement('button')
-    var del = document.createElement('button')
-    
+    var del  = document.createElement('button')
+    var head = document.createElement('span')
+
     title.innerHTML = "Title : " + this.name
     author.innerHTML = "Author : " + this.author
     pages.innerHTML = "Pages : " + this.pages
     read.innerHTML = this.read;
     del.innerHTML = "Delete"
+    head.innerHTML = "BOOK"
 
+    head.setAttribute("style", "font-size: 150%; color: var(--b)")
+    div.setAttribute("id", this.bookno)
+    del.setAttribute("onClick", "deletebook(this.parentNode.id)")
+    read.setAttribute("onClick", "changeText(this.parentNode.id)")
+
+    // del.addEventListener('click', function handleClick(event) {
+    //     console.log(event.target.parentElement.innerHTML);
+    //   });
+    // read.addEventListener('click', function handleClick(event) {
+    //     console.log(event.target.parentElement);
+    //   });
+
+    div.appendChild(head)
     div.appendChild(title)
     div.appendChild(author)
     div.appendChild(pages)
     div.appendChild(read)
     div.appendChild(del)
+
 
     return div
 
@@ -39,7 +77,9 @@ function display(){
 
     var right = document.getElementById('right');
 
-    for (var item in myLibrary){
+    right.innerHTML = ""
+
+    for (var item of myLibrary){
         var tag = item.info()
         right.appendChild(tag)
     }
@@ -53,13 +93,16 @@ form.addEventListener('submit', function(event){
     var title = document.getElementById('title').value
     var author = document.getElementById('author').value
     var pages = document.getElementById('pages').value
-    var cars = document.getElementById('read').value
-
-    const book = new Book(title, author, pages, cars)
+    var read = document.getElementById('read').value
+    console.log(read)
+    const book = new Book(title, author, pages, read, myLibrary.length)
+    
 
     addBookToLibrary(book);
 
     display();
+
+    form.reset();
     
 })
 
