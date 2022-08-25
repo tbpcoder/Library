8311c2310f5448c47e3b0ef44b8043a7bca3522d
thejas <thejas@cool.com>
2022-08-25 16:22:05 +0530

    modified index.js

--- index.js
+++ index.js
@@ -12,11 +12,55 @@ function addBookToLibrary(bookname) {
 }
 
 Book.prototype.info = function(){
-    return (`The ${this.name} by ${this.author}, ${this.pages} pages, ${this.read} yet`);
+    var div = document.createElement('div')
+    var title = document.createElement('p');
+    var author = document.createElement('p')
+    var pages = document.createElement('p')
+    var read = document.createElement('button')
+    var del = document.createElement('button')
+    
+    title.innerHTML = "Title : " + this.name
+    author.innerHTML = "Author : " + this.author
+    pages.innerHTML = "Pages : " + this.pages
+    read.innerHTML = this.read;
+    del.innerHTML = "Delete"
+
+    div.appendChild(title)
+    div.appendChild(author)
+    div.appendChild(pages)
+    div.appendChild(read)
+    div.appendChild(del)
+
+    return div
+
 }
 
-const book1 = new Book("Hobbit", "J.R.R. Tolkien", 295, "not read");
-addBookToLibrary(book1);
-console.log(book1.info());
-console.log(myLibrary);
+function display(){
+
+    var right = document.getElementById('right');
+
+    for (var item in myLibrary){
+        var tag = item.info()
+        right.appendChild(tag)
+    }
+
+}
+
+var form = document.getElementById('form')
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+    var title = document.getElementById('title').value
+    var author = document.getElementById('author').value
+    var pages = document.getElementById('pages').value
+    var cars = document.getElementById('read').value
+
+    const book = new Book(title, author, pages, cars)
+
+    addBookToLibrary(book);
+
+    display();
+    
+})
+
 
