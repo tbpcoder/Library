2b5a1949e034899d9861795c2a5b0fab1c541db5
thejas <thejas.bhat11@gmail.com>
2022-09-09 18:51:28 +0530

    Used classes

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-function Book(name, author, pages,  read, bookno){
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.bookno = bookno;
+class Book{
+    constructor(name, author, pages,  read, bookno){
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.bookno = bookno;   
+    }
+
+    info(){
+        var div = document.createElement('div')
+        var title = document.createElement('p');
+        var author = document.createElement('p')
+        var pages = document.createElement('p')
+        var read = document.createElement('button')
+        var del  = document.createElement('button')
+        var head = document.createElement('span')
+
+        title.innerHTML = "Title : " + this.name
+        author.innerHTML = "Author : " + this.author
+        pages.innerHTML = "Pages : " + this.pages
+        read.innerHTML = this.read;
+        del.innerHTML = "Delete"
+        head.innerHTML = "BOOK"
+
+        head.setAttribute("style", "font-size: 150%; color: var(--b)")
+        div.setAttribute("id", this.bookno)
+        del.setAttribute("onClick", "deletebook(this.parentNode.id)")
+        read.setAttribute("onClick", "changeText(this.parentNode.id)")
+
+        div.appendChild(head)
+        div.appendChild(title)
+        div.appendChild(author)
+        div.appendChild(pages)
+        div.appendChild(read)
+        div.appendChild(del)
+
+
+    return div
+
+    }
+
 }
 
 let myLibrary = [];
@@ -28,42 +64,7 @@ function changeText(bookid){
     }else{
         myLibrary[bookid].read = "Read"
     }
-
     display()
-
-}
-
-Book.prototype.info = function(){
-    var div = document.createElement('div')
-    var title = document.createElement('p');
-    var author = document.createElement('p')
-    var pages = document.createElement('p')
-    var read = document.createElement('button')
-    var del  = document.createElement('button')
-    var head = document.createElement('span')
-
-    title.innerHTML = "Title : " + this.name
-    author.innerHTML = "Author : " + this.author
-    pages.innerHTML = "Pages : " + this.pages
-    read.innerHTML = this.read;
-    del.innerHTML = "Delete"
-    head.innerHTML = "BOOK"
-
-    head.setAttribute("style", "font-size: 150%; color: var(--b)")
-    div.setAttribute("id", this.bookno)
-    del.setAttribute("onClick", "deletebook(this.parentNode.id)")
-    read.setAttribute("onClick", "changeText(this.parentNode.id)")
-
-    div.appendChild(head)
-    div.appendChild(title)
-    div.appendChild(author)
-    div.appendChild(pages)
-    div.appendChild(read)
-    div.appendChild(del)
-
-
-    return div
-
 }
 
 function display(){
@@ -89,8 +90,6 @@ form.addEventListener('submit', function(event){
     var read = document.getElementById('read').value
     console.log(read)
     const book = new Book(title, author, pages, read, myLibrary.length)
-    
-
     addBookToLibrary(book);
 
     display();
